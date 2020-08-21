80a9b2ccfab98b65ec0536655850f88d10bb6428
tamu <tamu6788@gmail.com>
2020-08-21 17:46:33 +0800

    项目第一天:测试查询所有不分页checkItem.html

---
+++ health_parent/health_web/src/main/java/com/itheima/health/controller/CheckItemController.java
@@ -0,0 +1,28 @@
+package com.itheima.health.controller;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.health.entity.Result;
+import com.itheima.health.pojo.CheckItem;
+import com.itheima.health.service.CheckItemService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import com.itheima.constant.MessageConstant;
+
+@RestController
+@RequestMapping("/checkItem")
+public class CheckItemController {
+
+    @Reference
+    private CheckItemService checkItemService;
+
+    @GetMapping("/findAll")
+    public Result findAll(){
+        List<CheckItem> checkItems = checkItemService.findAll();
+
+        return new Result(true,MessageConstant.QUERY_CHECKITEM_SUCCESS,checkItems);
+    }
+}
